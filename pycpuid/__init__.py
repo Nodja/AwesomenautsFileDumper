7167976a2dd0a47af52de000cae985706a7fa00e
Nodja <me@nodja.com>
2016-08-23 13:09:14 -0400

    add python3 compat cpuid

---
+++ pycpuid/__init__.py
@@ -0,0 +1,25 @@
+from .pycpuid import *
+
+__packagename__ = 'PyCPUID'
+__version__ = '0.5'
+__author__ = 'Bram de Greve'
+__author_email__ = 'bram.degreve@bramz.net'
+__maintainer__ = 'Flight Data Services Ltd'
+__maintainer_email__ = 'developers@flightdataservices.com'
+__url__ = 'http://pypi.python.org/pypi/PyCPUID'
+__description__ = 'CPUID powered by Python.'
+__download_url__ = 'https://github.com/FlightDataServices/PyCPUID'
+__classifiers__ = [
+    'Development Status :: 4 - Beta',
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: Implementation :: CPython',
+    'Operating System :: OS Independent',
+    'Topic :: Software Development',
+    ]
+__platforms__ = ['OS Independent']
+__license__ = 'GNU Library or Lesser General Public License (LGPL)'
+__keywords__ = ['cpuid']
---
+++ pycpuid/_pycpuid.c
@@ -0,0 +1,102 @@
+#include <Python.h>
+
+#ifdef _MSC_VER
+#include <intrin.h>
+#elif defined(__i386__) || defined(__x86_64__)
+#include <stdio.h>
+#include <sched.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+#include <cpuid.h>
+#endif
+
+static PyObject *_pycpuid_cpuid(PyObject* module, PyObject* args) {
+    #if defined(__i386__) || defined(__x86_64__)
+    pid_t pid;
+    cpu_set_t saved, target;
+    int affinity_ret, cpuid_ret;
+    #endif
+    int cpu_num = 0;
+    unsigned int level = 0;
+    unsigned int info[4] = { 0 };
+
+    if(!PyArg_ParseTuple(args, "I|i", &level, &cpu_num))
+        return NULL;
+
+    #ifdef _MSC_VER
+
+    /* Handling CPU affinity on Windows is not supported.  Patches welcome. */
+    __cpuid(info, level);
+    return Py_BuildValue("(IIII)", info[0], info[1], info[2], info[3]);
+
+    #elif defined(__i386__) || defined(__x86_64__)
+
+    pid = syscall(__NR_gettid);
+    if(sched_getaffinity(pid, sizeof(cpu_set_t), &saved) != 0) {
+        PyErr_SetString(PyExc_RuntimeError, "Unable to get CPU affinity");
+        return NULL;
+    }
+
+    CPU_ZERO(&target);
+    CPU_SET(cpu_num, &target);
+    if(sched_setaffinity(pid, sizeof(cpu_set_t), &target) != 0) {
+        PyErr_SetFromErrno(PyExc_OSError);
+        return NULL;
+    }
+
+    cpuid_ret = __get_cpuid(level, &info[0], &info[1], &info[2], &info[3]);
+    affinity_ret = sched_setaffinity(pid, sizeof(cpu_set_t), &saved);
+
+    if(cpuid_ret != 1) {
+        PyErr_SetString(PyExc_RuntimeError, "CPUID level not supported");
+        return NULL;
+    }
+
+    if(affinity_ret != 0) {
+        PyErr_SetFromErrno(PyExc_OSError);
+        return NULL;
+    }
+
+    return Py_BuildValue("(IIII)", info[0], info[1], info[2], info[3]);
+    #else
+    PyErr_SetString(PyExc_NotImplementedError, "CPUID is only supported on x86");
+    return NULL;
+    #endif
+}
+
+static PyMethodDef _pycpuid_methods[] = {
+    { "cpuid", _pycpuid_cpuid, METH_VARARGS, "cpuid(eax) -> (eax, ebx, ecx, edx)"},
+    { NULL, NULL, 0, NULL},
+};
+
+#if PY_MAJOR_VERSION >= 3
+
+static struct PyModuleDef moduledef = {
+    PyModuleDef_HEAD_INIT,
+    "_pycpuid",          /* m_name */
+    "CPUID info",        /* m_doc */
+    -1,                  /* m_size */
+    _pycpuid_methods,    /* m_methods */
+    NULL,                /* m_reload */
+    NULL,                /* m_traverse */
+    NULL,                /* m_clear */
+    NULL,                /* m_free */
+};
+
+PyMODINIT_FUNC PyInit__pycpuid(void) {
+    PyObject *m;
+
+    m = PyModule_Create(&moduledef);
+    if(m == NULL)
+        return NULL;
+    return m;
+}
+
+#else
+
+PyMODINIT_FUNC init_pycpuid(void) {
+    (void)Py_InitModule3("_pycpuid", _pycpuid_methods, "CPUID info");
+}
+
+#endif
+
